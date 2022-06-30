16b50cee45b92ee2f6d40d18fd25e5248fffd9de
VisShon <vishnu20414@iiitd.ac.in>
2022-07-01 04:18:32 +0530

    hero updated

--- pages/sections/Social.jsx
+++ pages/sections/Social.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import style from '../../styles/Social.module.css'
+import Button from '../../components/Button'
 
 function Social() {
   return (
-    <div className="h-screen w-screen">
+    <div className="w-screen h-screen bg-accent flex flex-col items-center font-mada font-[900] text-[4.8rem] w-[100%] leading-[4.8rem]">
 
+        <div className={style.Line} style={{marginTop:'0'}}></div>
+        <span className="text-textMain w-[30%] text-center text-[3.6rem] leading-[4.4rem]">So are they!
+          <span className="text-main"> EveryDay!</span>
+        </span>
+        <div>
+            <Button Content={'Become an early adopter!'} Link={''} />
+        </div>
+
+
+        <div className={style.Line} style={{marginBottom:'0'}}></div>
     </div>
   )
 }
