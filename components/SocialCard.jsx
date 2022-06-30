b6eada955220b002209a6602fe40b84c1fa92f45
VisShon <vishnu20414@iiitd.ac.in>
2022-06-30 23:25:03 +0530

    SocialCard updated

--- components/SocialCard.jsx
+++ components/SocialCard.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Image from 'next/Image'
 
-function SocialCard() {
+function SocialCard({ProfilePic, DiscordId, DiscordName, Review}) {
   return (
-    <div>SocialCard</div>
+    <div className="font-mada bg-white shadow-lg flex flex-col p-10 w-[10vw] rounded-lg">
+        <div className="flex">
+            <Image src={ProfilePic}/>
+            <div className="flex flex-col">
+                <p className="text-textMain">{DiscordName}</p>
+                <p className="text-textSecondary">{DiscordId}</p>
+            </div>
+        </div>
+
+        <p className="mt-10 text-textSecondary">
+            {Review}
+        </p>
+    </div>
   )
 }
 
