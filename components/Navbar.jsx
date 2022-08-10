36ed7c7b369f627bafa7b969a98f8667e43bc719
VisShon <vishnu20414@iiitd.ac.in>
2022-08-11 01:07:21 +0530

    small changes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Button from '../components/Button'
 import Logo from '../public/logo.png'
 import '../pages/_document'
+import Image from 'next/image'
 function Navbar() {
   return (
     <>
         <div className="flex justify-between items-center  p-10 font-Mada text-[1vw]">
-          <h1 className="font-mada font-[700] text-main text-[2.4rem]">revere</h1>
+          {/* <h1 className="font-mada font-[700] text-main text-[2.4rem]">revere</h1> */}
+          <Image src="/logo1.png" width={120} height={60}/>
           <div className="flex justify-between w-[35%] text-textMain text-[1rem] small:hidden">
              <a href="#Hero">About Us</a>
              <a href="#Social">Social</a>
--- pages/sections/Process.jsx
+++ pages/sections/Process.jsx
@@ -7,6 +7,7 @@ export default function Process() {
   return (
     <>
       <div id="Process" className="w-screen h-auto  flex justify-center text-[4.8rem] w-[100%] leading-[4.8rem] small:flex-col small:justfy-center ">
+        
         <div className={style.videoContainer}>
           <video loop autoPlay muted className={style.Video}>
                   <source src='vid.mp4' type='video/mp4'/>
